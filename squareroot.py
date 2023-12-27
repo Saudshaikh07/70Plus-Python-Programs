4e2409ddaa8b6e8005c3bc639e151356beb260d7
Saud Shaikh <shaikhsaud0@gmail.com>
2023-12-27 17:18:39 +0530

    Added squareroot python program

---
+++ squareroot.py
@@ -0,0 +1,13 @@
+#Calculating the square root using the ** arithmetic operator
+number = int(input("Enter a number here:"))
+squareroot = number ** (0.5)
+
+print("The square root of this number is:", squareroot)
+
+#Calculating square root using math module
+import math
+
+numberNew = int(input("Enter a number here: "))
+squarerootNew = math.sqrt(numberNew)
+
+print("The square root of this number is:", squareroot)
