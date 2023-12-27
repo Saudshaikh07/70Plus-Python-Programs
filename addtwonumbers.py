ff18a0b985d5c9ff4ea700a417384d1c4840910e
Saud Shaikh <shaikhsaud0@gmail.com>
2023-12-27 17:06:53 +0530

    Added addtwonumbers python program

---
+++ addtwonumbers.py
@@ -0,0 +1,17 @@
+# Add two numbers without taking inputs from the user
+
+num1 = 5
+num2 = 7
+
+sum = num1 + num2
+print("The sum of two numbers is: ", sum)
+
+
+#Add two numbers after taking input from the user
+
+num1 = float(input("Enter a number here: "))
+num2 = float(input("Enter another here: "))
+
+sum = num1 + num2
+print("The sum of the two numbers is: ", sum)
+
