b57051e3662380c4c40dca2f93fe121ea3808d4d
Saud Shaikh <shaikhsaud0@gmail.com>
2024-01-05 18:01:05 +0530

    Adding area of triangle program

---
+++ AreaOfTriangle.py
@@ -0,0 +1,9 @@
+#Program to calculate area of a triangle
+
+
+base = float(input("Enter the base of the triangle here: "))
+height = float(input("Enter the height of the triangle here: "))
+
+area = (0.5)*(base * height)
+
+print("Area of the triangle is: ", area)
