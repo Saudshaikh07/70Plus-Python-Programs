3ac4aacda73a8f9212a205f7ec67c198487ec48a
Saud Shaikh <shaikhsaud0@gmail.com>
2024-02-11 18:43:18 +0530

    Add a program to find decimal to Binary, Octal and Hexadecimal

---
+++ decimalToBinOctHex.py
@@ -0,0 +1,7 @@
+decimal = int(input("Enter a decimal number here: "))
+
+print("The decimal number -",decimal," in Binary is ", bin(decimal))
+
+print("The decimal number -",decimal," in Octal is ", oct(decimal))
+
+print("The decimal number -",decimal," in Hexadecimal is ", hex(decimal))
